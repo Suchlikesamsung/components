a7c7c700138d8525ef017f31295be36054d57654
suchlikesamsung <ncryko1@gmail.com>
2023-08-17 23:46:12 +0900

    누락파일추가

--- src/assets/components/card/card.js
+++ src/assets/components/card/card.js
@@ -1,5 +1,11 @@
-function CardComponent(){
-
+const CardComponent = (props) => {
+    <ul id="concepts">
+        <li className="concept">
+            <img src="TODO: IMAGE" alt="TODO: TITLE" />
+            <h2>TODO: TITLE</h2>
+            <p>TODO: DESCRIPTION</p>
+        </li>
+    </ul>
 
 }
 
