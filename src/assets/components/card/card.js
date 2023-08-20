9e816181b198f71f71e46890fe64c2e79dcaf127
suchlikesamsung <ncryko1@gmail.com>
2023-08-21 00:05:37 +0900

    2023-08-21

--- src/assets/components/card/card.js
+++ src/assets/components/card/card.js
@@ -1,11 +1,36 @@
+import "./card.css"
+import "../../images/contentimage_01.jpg"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const CardComponent = (props) => {
-    <ul id="concepts">
-        <li className="concept">
-            <img src="TODO: IMAGE" alt="TODO: TITLE" />
-            <h2>TODO: TITLE</h2>
-            <p>TODO: DESCRIPTION</p>
-        </li>
-    </ul>
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/post')
+            .then(
+                response => setData(response.data)
+            )
+            .catch(
+                error => console.error("API통신에러:", error)
+            )
+    })
+
+    console.log(data)
+
+
+    return <>
+        <ul id="concepts">
+            {data.map(item => (
+                <li key={item.id} className="concept">
+                    <img src="" alt={item.title} />
+                    <h2>{item.title}</h2>
+                    <p>{item.description}</p>
+                </li>
+            ))}
+        </ul>
+
+    </>
 
 }
 
--- src/assets/components/header/header.js
+++ src/assets/components/header/header.js
@@ -3,8 +3,8 @@ import "./header.css"
 const HeaderComponent = (props) => {
     return (
         <header>
-            <img src={props.data.image} alt="메달이미지" />
-            <h1>타이틀입니다.</h1>
+            <img src={props.data.image} alt="일반페페" />
+            <h1>블로그를 구성하는 컴포넌트</h1>
             <p>리액트 컴포넌트를 만들고 활용해봅시다.</p>
         </header>
     );
